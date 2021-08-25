62bc2a04024841e145a47079f9f358c0a4e8015d
michaelquintdev <michaelquintdev@gmail.com>
2021-08-25 10:23:57 -0500

    middleware

--- api/routers-models/bussiness/business-middleware.js
+++ api/routers-models/bussiness/business-middleware.js
@@ -1,8 +1,13 @@
-const confirmBusiness = (req, res, next) => {
+const Business = require('./business-model')
+
+const confirmBusiness = async (req, res, next) => {
+    const name = await Business.findBy({name: req.body.name})
     if(!req.body.name){
         res.status(401).json({message: 'Need to provide a business name'})
+    }else if(name.length > 0){
+        next({message: 'Business name is taken'})
     }else{
-        next()
+        next();
     }
 }
 
