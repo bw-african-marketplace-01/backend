f4a342aa6b3094e1cbd75eeb54888db9b82d58be
michaelquintdev <michaelquintdev@gmail.com>
2021-08-25 19:05:21 -0500

    items are added, edited tables once more

--- api/routers-models/Items/middleware.js
+++ api/routers-models/Items/middleware.js
@@ -1,3 +1,5 @@
+const Business = require('../bussiness/business-model')
+
 const validateItems = (req, res, next) => {
     const {category, location, name, description, price} = req.body
     if(!category || !location || !name || !description || !price ){
@@ -6,7 +8,23 @@ const validateItems = (req, res, next) => {
         next()
     }
 }
+const validateId = async(req, res, next) => {
+    try{
+        const {business_id} = req.body
+        if(!business_id){
+            next({status: 400, message: 'Please provide a business_id with your input'})
+        }
+        const check = await Business.findById(business_id)
+        if(check.length < 1){
+            next({status: 400, message: 'business_id does not exist in the database'})
+        }else{
+            next()
+        }
+    }catch(error){next(error)}
+    
+}
 
 module.exports = {
     validateItems,
+    validateId
 }
